af3f76b6e894c0dda5f2a40928fecaf5fbf3b296
Lorenzo Aloisi <aloisi.lorenzo99@gmail.com>
2023-11-24 20:07:03 +0100

    Update train_wddgan.py

--- train_wddgan.py
+++ train_wddgan.py
@@ -215,7 +215,7 @@ def train(rank, gpu, args):
 
             # sr and x(t+1) concat:
             x_tp1 = torch.add(sr_data, x_tp1)
-            x_tp1 = torch.div(tp_1, 2)
+            x_tp1 = torch.div(x_tp1, 2)
             print(x_tp1.size())
 
 
