b9339a81300daf0091cf2a526ff46893b8ae3b47
Lorenzo Aloisi <aloisi.lorenzo99@gmail.com>
2023-11-27 11:19:18 +0100

    testing inference

--- diffusion.py
+++ diffusion.py
@@ -171,7 +171,7 @@ def sample_from_model(coefficients, generator, n_time, x_init, x_cond, T, opt,):
             x_t_1 = torch.div(x_t_1, 2)
 
             x_0 = generator(x_t_1, t_time, latent_z)
-            x_new = sample_posterior(coefficients, x_0, x, t) # x(t-1) fake
+            x_new = sample_posterior(coefficients, x_0, x_t_1, t) # x(t-1) fake
             x = x_new.detach()
 
     return x
