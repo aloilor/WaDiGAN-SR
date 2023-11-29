db5ef8df8f16f62c761871b7fdeb695b01246759
Lorenzo Aloisi <aloisi.lorenzo99@gmail.com>
2023-11-29 21:05:02 +0100

    Update diffusion.py

--- diffusion.py
+++ diffusion.py
@@ -170,7 +170,7 @@ def sample_from_model(coefficients, generator, n_time, x_init, x_cond, T, opt,):
             x_t_1 = torch.cat([x, x_cond], dim=1)
 
             x_0 = generator(x_t_1, t_time, latent_z)
-            x_new = sample_posterior(coefficients, x_0, x_t_1, t) # x(t-1) fake
+            x_new = sample_posterior(coefficients, x_0, x, t) # x(t-1) fake
             x = x_new.detach()
 
     return x
