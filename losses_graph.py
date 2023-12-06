9345ce02970acaa17408989d8c1768c2a9422a7e
Lorenzo Aloisi <aloisi.lorenzo99@gmail.com>
2023-12-06 20:52:25 +0100

    update to losses graph

--- losses_graph.py
+++ losses_graph.py
@@ -4,60 +4,40 @@ import re
 
 # Modified loss values without iteration 0
 loss_data = """
-epoch 0 iteration100, G Loss: 27.645231246948242, D Loss: 1.7421889305114746
-epoch 0 iteration200, G Loss: 0.6217522621154785, D Loss: 1.7510735988616943
-epoch 0 iteration300, G Loss: 1.132054090499878, D Loss: 1.7664289474487305
-epoch 1 iteration100, G Loss: 0.9069512486457825, D Loss: 1.4492571353912354
-epoch 1 iteration200, G Loss: 2.8514368534088135, D Loss: 1.4047290086746216
-epoch 1 iteration300, G Loss: 0.7904479503631592, D Loss: 1.3965263366699219
-epoch 2 iteration100, G Loss: 3.1538679599761963, D Loss: 2.059518337249756
-epoch 2 iteration200, G Loss: 0.7567435503005981, D Loss: 1.447718858718872
-epoch 2 iteration300, G Loss: 0.7164775133132935, D Loss: 1.4030804634094238
-epoch 3 iteration100, G Loss: 0.8493850827217102, D Loss: 1.523701548576355
-epoch 3 iteration200, G Loss: 1.2424650192260742, D Loss: 1.4820970296859741
-epoch 3 iteration300, G Loss: 0.7453321218490601, D Loss: 1.4291741847991943
-epoch 4 iteration100, G Loss: 0.7377573251724243, D Loss: 1.3864597082138062
-epoch 4 iteration200, G Loss: 0.756231427192688, D Loss: 1.3824348449707031
-epoch 4 iteration300, G Loss: 0.737855076789856, D Loss: 1.3935389518737793
-epoch 5 iteration100, G Loss: 0.8647605776786804, D Loss: 1.4064245223999023
-epoch 5 iteration200, G Loss: 0.4370872974395752, D Loss: 1.527603030204773
-epoch 5 iteration300, G Loss: 0.6812267303466797, D Loss: 1.5698556900024414
-epoch 6 iteration100, G Loss: 0.7604415416717529, D Loss: 1.3871439695358276
-epoch 6 iteration200, G Loss: 0.7358837127685547, D Loss: 1.8417712450027466
-epoch 6 iteration300, G Loss: 0.745964765548706, D Loss: 1.3893699645996094
-epoch 7 iteration100, G Loss: 0.7356603145599365, D Loss: 1.3853836059570312
-epoch 7 iteration200, G Loss: 0.054331351071596146, D Loss: 1.810903549194336
-epoch 7 iteration300, G Loss: 0.7262622117996216, D Loss: 1.388880729675293
-epoch 8 iteration100, G Loss: 0.7336065769195557, D Loss: 1.3930189609527588
-epoch 8 iteration200, G Loss: 0.7030291557312012, D Loss: 1.386734962463379
-epoch 8 iteration300, G Loss: 0.7273914813995361, D Loss: 1.384037971496582
-epoch 9 iteration100, G Loss: 0.7173991203308105, D Loss: 1.3855931758880615
-epoch 9 iteration200, G Loss: 0.6985913515090942, D Loss: 1.3871817588806152
-epoch 9 iteration300, G Loss: 0.7149405479431152, D Loss: 1.386453628540039
-epoch 10 iteration100, G Loss: 0.7426645755767822, D Loss: 1.3835349082946777
+epoch 0 iteration100, G Loss: 0.622607409954071, D Loss: 1.4010212421417236
+epoch 0 iteration200, G Loss: 0.7272967100143433, D Loss: 1.386047124862671
+epoch 0 iteration300, G Loss: 0.7401588559150696, D Loss: 1.397234320640564
+epoch 1 iteration100, G Loss: 0.6124520897865295, D Loss: 1.4474921226501465
+epoch 1 iteration200, G Loss: 0.6810575723648071, D Loss: 1.3866417407989502
+epoch 1 iteration300, G Loss: 0.7303689122200012, D Loss: 1.3862459659576416
+epoch 2 iteration100, G Loss: 0.7124195694923401, D Loss: 1.38873291015625
+epoch 2 iteration200, G Loss: 0.7089831233024597, D Loss: 1.387303113937378
+epoch 2 iteration300, G Loss: 0.874885618686676, D Loss: 1.3874375820159912
+epoch 3 iteration100, G Loss: 0.3985002338886261, D Loss: 1.387665033340454
+epoch 3 iteration200, G Loss: 0.7266682982444763, D Loss: 1.3862099647521973
+epoch 3 iteration300, G Loss: 0.7378641963005066, D Loss: 1.3856427669525146
+epoch 4 iteration100, G Loss: 0.6824491024017334, D Loss: 1.387473225593567
+epoch 4 iteration200, G Loss: 0.7031933069229126, D Loss: 1.3891931772232056
+epoch 4 iteration300, G Loss: 0.7208313345909119, D Loss: 1.385925531387329
+epoch 5 iteration100, G Loss: 0.7209826111793518, D Loss: 1.3860039710998535
 """
 
 # Parse loss data and extract generator and discriminator loss
 lines = loss_data.strip().split('\n')
-print(len(lines))
 generator_loss = [float(line.split(', ')[1].split(': ')[1]) for line in lines]
 discriminator_loss = [float(line.split(', ')[2].split(': ')[1]) for line in lines]
 
 iterations = [int(match.group(1)) for match in re.finditer(r'iteration(\d+)', loss_data)]
 num_iterations = len(iterations)  # Adjust this based on your needs
 
-print(num_iterations)
-
 iterations = [100 + 100 * i for i in range(num_iterations)]
-print(iterations)
-
 
 
 # Plotting the generator and discriminator loss
 plt.figure(figsize=(10, 6))
 plt.plot(iterations, generator_loss, label='Generator Loss', marker='o', linestyle='-', markersize=5)
 plt.plot(iterations, discriminator_loss, label='Discriminator Loss', marker='o', linestyle='-', markersize=5)
-plt.title('Generator and Discriminator Loss Over Cumulative Iterations')
+plt.title('Generator and Discriminator Loss Over Iterations')
 plt.xlabel('Iterations')
 plt.ylabel('Loss')
 plt.legend()
