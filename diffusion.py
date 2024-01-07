69ee2fbe7a2fbc650cc213b86950abbe39012a05
Lorenzo Aloisi <aloisi.lorenzo99@gmail.com>
2024-01-07 19:27:16 +0100

    trying to save diffusion process

--- diffusion.py
+++ diffusion.py
@@ -174,6 +174,7 @@ def sample_from_model(coefficients, generator, n_time, x_init, x_cond, T, opt,):
 
             x_0 = generator(x_t_1, t_time, latent_z)
             x_new = sample_posterior(coefficients, x_0, x, t) # x(t-1) fake
-            x = x_new.detach()
+            #x = x_new.detach()
+            x = torch.cat([x,x_new.detach()], dim=0)
 
     return x
