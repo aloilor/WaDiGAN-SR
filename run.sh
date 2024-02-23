e6f930b571dd8e93ca91cf895d9c615de4dc93dc
Lorenzo Aloisi <aloisi.lorenzo99@gmail.com>
2024-02-23 10:09:02 +0100

    fixing batch size

--- run.sh
+++ run.sh
@@ -45,7 +45,7 @@ if [[ $MODE == train ]]; then
 	elif [[ $DATASET == div2k_128_512 ]]; then 
 		python train_srwddgan.py --dataset div2k_128_512 --image_size 512 --exp div2k_128_512_batch16_ts4_atn16_wg12224_d6_recloss_200ep --num_channels 24 \
 			--num_channels_dae 64 --ch_mult 1 2 2 2 4 --num_timesteps 4 \
-			--num_res_blocks 2 --batch_size 4 --num_epoch 200 --ngf 64 --embedding_type positional --use_ema --r1_gamma 2. \
+			--num_res_blocks 2 --batch_size 2 --num_epoch 200 --ngf 64 --embedding_type positional --use_ema --r1_gamma 2. \
 			--cond_emb_dim 256 --lr_d 1e-4 --lr_g 1.6e-4 --lazy_reg 10 --save_content \
 			--datadir /content/gdrive/MyDrive/srwavediff/datasets/div2k_128_512/ \
 			--master_port $MASTER_PORT \
